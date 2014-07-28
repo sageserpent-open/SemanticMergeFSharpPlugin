a88cb1e7129f9396ff2b967436fe711f88c46fd2
gerardMurphy <gerardMurphy@.(none)>
2014-07-28 16:43:59 +0100

    Painfully gaining ground - fighting lack of documentation on FSharp parser's range support  - am now isolating the header for a module (turns out the headers are mandatory).

--- StructureDiscovery/FileProcessor.fs
+++ StructureDiscovery/FileProcessor.fs
@@ -5,6 +5,7 @@ open Microsoft.FSharp.Compiler.Range
 open Microsoft.FSharp.Compiler.Ast
 open System.IO
 open System
+open System.Text.RegularExpressions
 
 module FileProcessor = 
     type LineSpan = (int * int) * (int * int) // First item in sub-pair is a one-relative line position,
@@ -51,13 +52,45 @@ module FileProcessor =
           LocationSpan: LineSpan
           FooterSpan: CharacterSpan
           Children: List<Section>
-          ParsingErrors: array<ParsingError>
-                                             }
+          ParsingErrors: array<ParsingError> }
+    
+    let private lineRegex = Regex(@"^.*$", RegexOptions.Multiline)
     
     let DiscoverStructure(pathOfInputFile: string, 
                           pathOfOutputFileForYamlResult: string) = 
         let contentsOfInputFile = File.ReadAllText(pathOfInputFile)
         let interactiveChecker = InteractiveChecker.Create()
+        
+        let onePastLineBreakPositionsInInputFile = 
+            // HACK: I'm not aware of any way of prising open the 'System.IO.File.ReadAllLines()' abstraction
+            // that would give a platform- / file line ending - independent way of seeing where the lines start
+            // within the complete string of characters in the file, including any multi-character EOLs. So this
+            // is my best attempt at doing something pithy. Hmm.
+            let lineMatches = lineRegex.Matches contentsOfInputFile
+            [| for lineMatch in lineMatches do
+                   yield lineMatch.Index |]
+        
+        //        let lineAndColumnIndicesFor characterPosition =
+        //            let searchResult = Array.BinarySearch(onePastLineBreakPositionsInInputFile, characterPosition)
+        //            let lineIndex =
+        //                if 0 > searchResult
+        //                then
+        //                    ~~~searchResult - 1
+        //                else
+        //                    searchResult
+        //            lineIndex, characterPosition - onePastLineBreakPositionsInInputFile.[lineIndex]
+        let totalNumberOfLines = onePastLineBreakPositionsInInputFile.Length
+        let characterSpanFor (range: range) = 
+            let characterPositionFor oneRelativeLinePosition columnIndex = 
+                let onePastLineBreakPositionInInputFile = 
+                    onePastLineBreakPositionsInInputFile.[oneRelativeLinePosition - 1]
+                onePastLineBreakPositionInInputFile + columnIndex
+            characterPositionFor range.StartLine range.StartColumn, 
+            characterPositionFor range.EndLine range.EndColumn - 1
+        
+        let locationSpanFor (range: range) = 
+            (range.StartLine, range.StartColumn), 
+            (range.EndLine, range.EndColumn)
         
         let sections, parsingErrors = 
             let fakeProjectOptions = 
@@ -77,26 +110,36 @@ module FileProcessor =
                                                  let message = error.Message
                                                  { Location = location
                                                    Message = message })
-            let longIdentifierFrom (longIdentifierPieces: LongIdent) =
+            
+            let longIdentifierFrom (longIdentifierPieces: LongIdent) = 
                 String.Join(".", longIdentifierPieces)
             match resultsFromParsing.ParseTree with
-            | Some(ParsedInput.ImplFile (ParsedImplFileInput (_, _, _, _, _, modulesOrNamespaces, _))) ->
-                let sectionFromModuleOrNamespace moduleOrNameSpace =
+            | Some(ParsedInput.ImplFile(ParsedImplFileInput(_, _, _, _, _, 
+                                                            modulesOrNamespaces, 
+                                                            _))) -> 
+                let sectionFromModuleOrNamespace moduleOrNameSpace = 
                     match moduleOrNameSpace with
-                        SynModuleOrNamespace (longIdentifierPieces, isModule, containedDeclarations, _, _, _, range) ->
-                            let rangeOfContainedDeclarations = (containedDeclarations |> List.map (fun declaration -> declaration.Range) |> List.reduce unionRanges)
-                            let overallRange = unionRanges rangeOfContainedDeclarations range
-                            let container =
-                                {
-                                    Type = if isModule then "module" else "namespace"
-                                    Name = longIdentifierFrom longIdentifierPieces
-                                    LocationSpan = (overallRange.StartLine, overallRange.StartColumn), (overallRange.EndLine, overallRange.EndColumn)
-                                    HeaderSpan = emptyCharacterSpan
-                                    FooterSpan = emptyCharacterSpan
-                                    Children = List.Empty
-                                }
-                            Container container
-                modulesOrNamespaces |> List.map sectionFromModuleOrNamespace, parsingErrors
+                    | SynModuleOrNamespace(longIdentifierPieces, isModule, 
+                                           containedDeclarations, _, _, _, overallRange) -> 
+//                        let rangeOfContainedDeclarations = 
+//                            (containedDeclarations
+//                             |> List.map (fun declaration -> declaration.Range)
+//                             |> List.reduce unionRanges)
+
+                        let headerRange = mkRange String.Empty overallRange.Start (Seq.last longIdentifierPieces).idRange.End
+                        
+                        let container = 
+                            { Type = 
+                                  if isModule then "module"
+                                  else "namespace"
+                              Name = longIdentifierFrom longIdentifierPieces
+                              LocationSpan = locationSpanFor overallRange
+                              HeaderSpan = characterSpanFor headerRange
+                              FooterSpan = emptyCharacterSpan
+                              Children = List.Empty }
+                        Container container
+                modulesOrNamespaces |> List.map sectionFromModuleOrNamespace, 
+                parsingErrors
             | _ -> 
                 raise 
                     (Exception
@@ -117,8 +160,7 @@ module FileProcessor =
               Children = sections
               ParsingErrors = parsingErrors }
         
-        let yamlForOverallStructure { Name = name; 
-                                      LocationSpan = locationSpan; 
+        let yamlForOverallStructure { Name = name; LocationSpan = locationSpan; 
                                       FooterSpan = footerSpan; 
                                       Children = children; 
                                       ParsingErrors = parsingErrors } = 
@@ -127,6 +169,10 @@ module FileProcessor =
                 String.Format
                     ("{{start: [{0},{1}], end: [{2},{3}]}}", startLine, 
                      indexOfFirstCharacter, endLine, indexOfOnePastLastCharacter)
+            let yamlForCharacterSpan (indexOfFirstCharacter, 
+                                      indexOfLastCharacter) = 
+                String.Format
+                    ("[{0}, {1}]", indexOfFirstCharacter, indexOfLastCharacter)
             let yamlForEmptyCharacterSpan = "[0, -1]"
             let indent indentationLevel line = 
                 String.replicate indentationLevel " " + line
@@ -138,10 +184,10 @@ module FileProcessor =
             
             and yamlForSection section = 
                 let yamlForContainer { Type = typeName; Name = name; 
-                                           LocationSpan = locationSpan; 
-                                           HeaderSpan = headerSpan; 
-                                           FooterSpan = footerSpan; 
-                                           Children = children } = 
+                                       LocationSpan = locationSpan; 
+                                       HeaderSpan = headerSpan; 
+                                       FooterSpan = footerSpan; 
+                                       Children = children } = 
                     [ yield String.Format("- type : {0}", typeName)
                       yield String.Format("  name : {0}", name)
                       
@@ -150,7 +196,8 @@ module FileProcessor =
                                  yamlForLineSpan locationSpan)
                       
                       yield String.Format
-                                ("  headerSpan : {0}", yamlForEmptyCharacterSpan)
+                                ("  headerSpan : {0}", 
+                                 yamlForCharacterSpan headerSpan)
                       
                       yield String.Format
                                 ("  footerSpan : {0}", yamlForEmptyCharacterSpan)
@@ -178,7 +225,9 @@ module FileProcessor =
                                       Message = message } = 
                 [ yield String.Format
                             ("- location: [{0},{1}]", line, indexOfCharacter)
-                  yield String.Format("  message: \"{0}\"", message.Replace("\"", "\\\"")) ]
+                  
+                  yield String.Format
+                            ("  message: \"{0}\"", message.Replace("\"", "\\\"")) ]
             
             let pieces = 
                 [ yield "---"
@@ -206,7 +255,9 @@ module FileProcessor =
             
             joinPiecesOnSeparateLines pieces
         
-        let yamlResult =
-            yamlForOverallStructure overallStructure
+        let yamlResult = yamlForOverallStructure overallStructure
         File.WriteAllText(pathOfOutputFileForYamlResult, yamlResult)
-        File.WriteAllText(String.Format(@"F:\{0}.txt", Path.GetFileNameWithoutExtension(pathOfInputFile)), yamlResult)
+        File.WriteAllText
+            (String.Format
+                 (@"F:\{0}.txt", 
+                  Path.GetFileNameWithoutExtension(pathOfInputFile)), yamlResult)
